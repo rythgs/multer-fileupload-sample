4b6e0bad39c8df4d9e6e6eb3497995b710b16289
rythgs <me@rythgs.co>
2018-01-11 22:22:14 +0900

    add body-parser

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const multer = require('multer');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const fileFilter = function(req, file, cb) {
   if (!file.originalname.match(/\.(jpg|jpeg|gif|png)$/)) {
